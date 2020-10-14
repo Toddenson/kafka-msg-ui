a009a0f05c96ac16bca4b92eec9fa1df9eed4c40
Oleksii Gordieiev <algordyev@gmail.com>
2020-10-14 13:41:33 +0300

    add sent and received messages cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatListModule,
+        MatCardModule,
+      MatButtonModule,
+      MatToolbarModule,
+      MatIconModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
